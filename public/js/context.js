bac2592339647cc1304b66a0b66a348c15f8d62a
atrifan <trifan.alex.criss@gmail.com>
2016-02-01 22:50:18 +0200

    updated logics + font support

--- public/js/context.js
+++ public/js/context.js
@@ -1,7 +1,8 @@
 define(['componentMap',
         'promise',
-        'componentRequester'
-        ], function (ComponentMap, Promise, ComponentRequester) {
+        'componentRequester',
+        'sockets'
+        ], function (ComponentMap, Promise, ComponentRequester, Socket) {
 
     /**
      * The component's controller execution context this module is being injected in the controller
@@ -59,6 +60,15 @@ define(['componentMap',
      */
     Context.prototype.getRoot = function () {
         return $('#' + this._parentId);
+    };
+
+    /**
+     * Connects to server via webSockets on a channel inputed by the developer
+     * @param channel
+     * @returns {*}
+     */
+    Context.prototype.getSocketToServer = function (channel) {
+        return Socket.get().registerSocket(channel)
     };
 
     /**
