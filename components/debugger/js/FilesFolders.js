a3426efa0f8c4409e4f20aa3d92d59131fdfaef3
trifan <trifan@adobe.com>
2018-04-03 22:59:30 +0300

    debugger

---
+++ components/debugger/js/FilesFolders.js
@@ -0,0 +1,31 @@
+/**
+ * Created by atrifan on 10/28/2015.
+ */
+define(['modal'], function (Modal) {
+    function FilesFolders() {
+
+    }
+
+    FilesFolders.prototype.init = function () {
+        // var self = this;
+        // this._socket = this.context.getSocketToServer('wordGame');
+        // this._socket.on('registerSelf', function(okState) {
+        //     if(okState) {
+        //         self._showGame();
+        //     }
+        // });
+        // this._socket.on('registerUser', function(user) {
+        //     self._appendUser(user);
+        // });
+    };
+
+    FilesFolders.prototype.start = function () {
+        // this._root = this.context.getRoot();
+        // this._gameWrapper = this._root.find('.wordGameWrapper');
+        // this._usersContainer = this._root.find('.users-container');
+        // this._chooseUserName();
+
+    }
+
+    return FilesFolders;
+});
